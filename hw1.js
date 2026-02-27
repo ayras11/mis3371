044598e4aa6d79b3e9420838f3d035483818b224
ayras11 <ayra.n.siddiqui@gmail.com>
2026-02-27 14:23:40 -0600

    Add files via upload

---
+++ hw1.js
@@ -0,0 +1,12 @@
+//dynamic date js code
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("today").innerHTML = text;
+//name slider js code
+let slider = document.getElementById("range");
+let output = document.getElementById("range-slider");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+};
